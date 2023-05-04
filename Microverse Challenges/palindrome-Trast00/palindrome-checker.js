d4e4c6574fa98bd88cb10034af18663d343aa515
Allassane <dickoallassane1997@gmail.com>
2023-05-04 09:06:15 +0100

    Remove all files

--- Microverse Challenges/palindrome-Trast00/palindrome-checker.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (str) {
-  // write your code here
-  str = str.toLowerCase().replace(/[^a-z0-9]/gi, '').replaceAll(" ", "")
-  
-  if(str!=str.split("").reverse().join("")){
-    return false
-  }
-  return true
-}
