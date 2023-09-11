275751ce7e309387a707476de683360d07ab3b51
Allassane <dickoallassane1997@gmail.com>
2023-09-11 19:15:36 +0100

    Add product arra first try

---
+++ LeetCode/Product Array/challenge.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        # create a result_hash, hashmap with: key=> index, and value => products
+        # for each num in nums
+            # if num exist in result_hash
+                # result_hash[i] = 
+            # else 
